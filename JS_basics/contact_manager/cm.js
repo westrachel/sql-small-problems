b5d867079c7ef60a81f1e76a5dcf39f373ae3ac9
EC2 Default User <ec2-user@ip-172-31-4-26.us-east-2.compute.internal>
2022-09-30 15:55:36 +0000

    worked on implementation of initial event listeners for toggling hide class of certain button and form elements of contact manager app

--- JS_basics/contact_manager/cm.js
+++ JS_basics/contact_manager/cm.js
@@ -3,24 +3,51 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const contactManager = {
-  
   arrify: function(collection) {
     return Array.prototype.slice.call(collection);
   },
 
-  unhideEls: function(...elements) {
-    elements.forEach(element => {
-      element.classList.remove('hide');
+  setDisplayStatus: function(...statusAndEls) {
+    const display = statusAndEls[0];
+    const els = statusAndEls.slice(1);
+  
+    els.forEach(element => {
+      if (display === 'hide') {
+        element.classList.add('hide');
+      } else {
+        element.classList.remove('hide');
+      }
     });
+  },
+
+  changeAddFormDisplay: function(showForm) {
+    const formStatus = showForm === 'unhide' ? 'unhide' : 'hide';
+    const addBtnsStatus = showForm === 'unhide' ? 'hide' : 'unhide';
+
+    this.setDisplayStatus(formStatus, this.addContactForm);
+    this.setDisplayStatus(addBtnsStatus, this.addContactBtns, this.searchField);
   },
 
   init: function() {
     this.addContactBtns = this.arrify(document.getElementsByClassName('btn btn-lg btn-outline'));
     this.addContactForm = document.querySelector('#contact-form-info');
+    this.searchField = document.querySelector('.search')
+    this.cancelBtn = document.querySelector('button');
+    this.bindEvents();
   },
   
   bindEvents: function() {
-    this.addContactBtns.addEventListener('click', this.unhideEls(this.addContactForm));
+    let self = this;
+    self.addContactBtns.forEach(button => {
+      addEventListener('click', () => {
+        self.changeAddFormDisplay('unhide');
+      });
+    });
+    
+    self.cancelBtn.addEventListener('click', () => {
+      alert('hide form!');
+      self.changeAddFormDisplay('hide');
+    });
   }
 
 };
