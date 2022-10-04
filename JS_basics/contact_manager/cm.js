61819860f2a193d29e532f60ab6152f1c5de49f6
EC2 Default User <ec2-user@ip-172-31-4-26.us-east-2.compute.internal>
2022-10-04 14:30:34 +0000

    refactored to incorporate more event delegation and worked on implementing edit form functionality

--- JS_basics/contact_manager/cm.js
+++ JS_basics/contact_manager/cm.js
@@ -4,20 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const addContactForm = {
       id: 'add-contact-form',
       title: 'Create Contact',
-      name_placeholder: '',
-      email_placeholder: '',
-      phone_placeholder: '',
-      sales_selected: '',
-      marketing_selected: '',
-      engineering_selected: '',
-      hr_selected: ''
+      fullname: '',
+      email: '',
+      phone_number: '',
+      sales: '',
+      marketing: '',
+      engineering: '',
+      hr: ''
     };
     
+    const VALID_IDS = ["fullname", "email", "phone_number", "tag"];
     const editForm = Object.assign({}, addContactForm);
     editForm.title = 'Edit Contact';
     editForm.id = 'edit-contact-form';
     
-    const templates = {
+    const TEMPLATES = {
       form: Handlebars.compile(document.querySelector('#contact-form-template').innerHTML),
       contacts: Handlebars.compile(document.querySelector("#contacts-template").innerHTML)
     };
@@ -32,18 +33,26 @@ document.addEventListener('DOMContentLoaded', () => {
         return Array.prototype.slice.call(collection);
       },
   
-      toggleDisplay: function(element) {
-        element.classList.toggle('hide');
+      showHide: function(elsToHide, elsToUnhide) {
+        elsToHide.forEach(el => {
+          if (!el.classList.contains('hide')) {
+            el.classList.add('hide');
+          }
+        });
+        
+        elsToUnhide.forEach(el => {
+          el.classList.remove('hide');
+        });
       },
-      
-      addInputForm: function() {
-        let html = templates.form(addContactForm);
+
+      addHTMLForm: function(formObject) {
+        let html = TEMPLATES.form(formObject);
         let parent = document.querySelector('.main-container');
         parent.innerHTML += html;
       },
 
       init: function() {
-        this.addInputForm();
+        this.addHTMLForm(addContactForm);
         this.addForm = document.querySelector('#add-contact-form');
         this.search = document.querySelector('.search');
         this.addSearchHeader = document.querySelector('.add-search-container');
@@ -57,7 +66,6 @@ document.addEventListener('DOMContentLoaded', () => {
     
       createContact: function(form) {
         const contact = { id: generateContactId() };
-        const VALID_IDS = ["fullname", "email", "phone_number", "tag"];
 
         this.arrify(form).forEach(element => {
           if (VALID_IDS.includes(element.id)) {
@@ -72,16 +80,20 @@ document.addEventListener('DOMContentLoaded', () => {
           return char.match('[0-9]');
         }).join();
       },
+
+      forms: function() {
+        return this.arrify(document.querySelectorAll('form'));
+      },
   
       bindEvents: function() {
         const self = this;
         document.addEventListener('click', event => {
           event.preventDefault();
           const target = event.target;
-          
+
           if (target.className.match('add-contact')) {
-            const changeDisplayEls = [self.addSearchHeader, self.addForm, self.emptyContacts];
-            changeDisplayEls.forEach(self.toggleDisplay);
+            self.showHide([self.addSearchHeader, self.emptyContacts, self.divOfContacts], 
+                          [self.addForm]);
 
           } else if (target.nodeName === 'A') {
             const deleteMatch = target.href.match('delete/');
@@ -95,22 +107,43 @@ document.addEventListener('DOMContentLoaded', () => {
                 target.parentNode.parentNode.remove();
                 
                 if (self.allContacts.length === 0) {
-                  self.toggleDisplay(self.emptyContacts);
+                  self.showHide([self.forms], [self.emptyContacts]);
                 }
               }
             } else if (editMatch) {
+              const id = Number(self.anchorContactId(editMatch, target.href));
+              const contact = self.allContacts.filter(contact => contact.id === id);
+              
+              const selectedTags = Object.keys(editForm).filter(key => {
+                key.match('(sales|marketing|engineering|hr)');
+              });
+              selectedTags.forEach(tag => editForm[tag] = '');
+
+              VALID_IDS.forEach(property => {
+                if (property !== 'tag') {
+                  editForm[property] = "placeholder='" + contact[property] + "'";
+                } else {
+                  editForm[contact[property]] = "selected";
+                }
+              });
+              
+              Object.keys(editForm).forEach(l => console.log(editForm[l]));
+              self.addHTMLForm(editForm);
+              self.showHide([self.addSearchHeader, self.divOfContacts],
+                            [document.querySelector('#edit-contact-form')]);
             }
+          } else if (target.innerHTML.match('Submit')) {
+              self.allContacts.push(self.createContact(self.addForm));
+              self.showHide([self.addForm], [self.addSearchHeader, self.divOfContacts]);
+          
+              let htmlData = TEMPLATES.contacts({ contacts: self.allContacts });
+              self.divOfContacts.innerHTML += htmlData;
+          } else if (target.innerHTML.match('Cancel')) {
+            const unHideEls = [self.addSearchHeader, self.divOfContacts];
+            if (self.allContacts.length === 0) { unHideEls.push(self.emptyContacts); }
+            self.showHide(self.forms, unHideEls);
           }
           
-        });
-
-        self.addForm.addEventListener('submit', event => {
-          event.preventDefault();
-          self.allContacts.push(self.createContact(self.addForm));
-          [self.addBtns[0], self.search, self.addForm].forEach(self.toggleDisplay);
-          
-          let htmlData = templates.contacts({ contacts: self.allContacts});
-          self.divOfContacts.innerHTML += htmlData;
         });
         
         
